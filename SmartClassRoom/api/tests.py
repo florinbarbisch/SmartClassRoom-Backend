76d70ec0544ec82a8445e8a3aa555cddf9f7ae58
Etienne Roulet <32195170+etiiiR@users.noreply.github.com>
2022-03-27 00:41:15 +0100

    Update tests.py

--- SmartClassRoom/api/tests.py
+++ SmartClassRoom/api/tests.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from .models import Classroom, MeasurementStation, Measurement, ConnectionHistory
 
 class Selinum_Test_Cases(TestCase):
-    #Well i hate python for Web Testing :) Cypress is the best
+    #Well python is not suited for Web Testing but whatever :) Cypress
     def setUp(self):
         try:
             self.driver = webdriver.Chrome('C:\\tools\selenium\chromedriver.exe')
