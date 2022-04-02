e5dd7e5844885884ddc31f4575a3039432e37552
Etienne Roulet <32195170+etiiiR@users.noreply.github.com>
2022-04-02 20:52:59 +0200

    Update settings.py
    for simlicity

--- SmartClassRoom/SmartClassRoom/settings.py
+++ SmartClassRoom/SmartClassRoom/settings.py
@@ -68,7 +68,7 @@ SIMPLE_JWT = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 999,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
